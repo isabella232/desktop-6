0a10edcb04947a830d0d1d21bce7863b2184dd94
René Preuß <rene@preuss.io>
2020-12-03 13:10:47 +0100

    Add jukebox details

--- preload.js
+++ preload.js
@@ -1,6 +1,8 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
+  window.app = true;
+
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
